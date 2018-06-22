6e02a41b4ab04dc22481e7322e043de5b27adec7
Unknown <jamhudsp@uat.edu>
2018-06-22 13:25:29 -0700

    Interact sync test
    added master sync script

---
+++ Meltdown/Assets/Scripts/MasterSync.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NetBase;
+
+public class MasterSync : MonoBehaviour {
+
+	public bool isMaster = false;
+
+	[Tooltip("Photon View")]
+	public PhotonView observed;
+	[Tooltip("Network Object")]
+	public NetworkObject toObserev;
+
+	// Use this for initialization
+	void Start () {
+		observed = GetComponent<PhotonView> ();
+		toObserev = GetComponent<NetworkObject> ();
+		ObserveTheObject ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void ObserveTheObject() {
+
+		if (PhotonNetwork.isMasterClient)
+		{
+			observed.ObservedComponents.Add (toObserev);
+			isMaster = true;
+		}
+	}
+}
